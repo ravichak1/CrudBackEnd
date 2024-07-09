5365136e153b1c68a649514f97b00329948e2df6
Kirthigesan <kirthi101996@gmail.com>
2024-07-09 10:56:25 +0200

    add

--- config/dbConnection.js
+++ config/dbConnection.js
@@ -1,6 +1,6 @@
-const mongoose= require("mongoose")
+const mongoose = require("mongoose");
 
 mongoose
-.connect(process.env.MONGO_DB_URL)
-.then((db) => console.log(`DB connected: ${db.connection.name}`))
-.catch((e) => console.log(e));
+  .connect(process.env.MONGODB_URI)
+  .then((db) => console.log(`DB connected: ${db.connection.name}`))
+  .catch((e) => console.log(e));
--- controller/userController/login.js
+++ controller/userController/login.js
@@ -1,43 +1,44 @@
 //description Create user
 //route  POST api/users
 //access public
-const User=require('../../models/User.models')
-const bcrypt=require("bcryptjs")
-const jwt=require("jsonwebtoken")
-async function login(req,res){
-    try {
-        const { username, password } = req.body;
-    
-        if (!username || !password) {
-          return res
-            .status(400)
-            .json({ message: "Username and password are required" });
-        }
-    
-        const foundUser = await User.findOne({ username: username });
-        if (!foundUser) {
-          return res.status(401).json({ message: "Invalid username or password" });
-        }
-    
-        const correctPassword = bcrypt.compare(password, foundUser.password);
-        if (!correctPassword) {
-          return res.status(401).json({ message: "Invalid username or password" });
-        }
-    
-        const payload = { id: foundUser._id };
-        const token = jwt.sign(payload, process.env.TOKEN_SECRET, {
-          expiresIn: "1d",
-          algorithm: "HS256",
-        });
-    
-        res.json({
-          accesstoken: token,
-          userId: foundUser._id, userName: foundUser.username 
-        })
-    }catch (error){
-        console.log(error)
+const User = require("../../models/User.models");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+async function login(req, res) {
+  try {
+    const { username, password } = req.body;
+
+    if (!username || !password) {
+      return res
+        .status(400)
+        .json({ message: "Username and password are required" });
     }
+
+    const foundUser = await User.findOne({ username: username });
+    if (!foundUser) {
+      return res.status(401).json({ message: "Invalid username or password" });
+    }
+
+    const correctPassword = bcrypt.compare(password, foundUser.password);
+    if (!correctPassword) {
+      return res.status(401).json({ message: "Invalid username or password" });
+    }
+
+    const payload = { id: foundUser._id };
+    const token = jwt.sign(payload, process.env.TOKEN_SECRET, {
+      expiresIn: "1d",
+      algorithm: "HS256",
+    });
+
+    res.json({
+      accesstoken: token,
+      userId: foundUser._id,
+      userName: foundUser.username,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(400).json({ message: "havent found" });
+  }
 }
 
-
-module.exports = {login}
+module.exports = { login };
